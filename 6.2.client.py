c57b2271fc21a53697b41e3fbeecff348b530794
syaheerLGD <73862545+syaheerLGD@users.noreply.github.com>
2020-12-27 13:23:30 +0800

    Add files via upload

---
+++ 6.2.client.py
@@ -0,0 +1,25 @@
+import socket
+
+ClientSocket = socket.socket()
+host = '192.168.0.200'
+port = 8888
+
+print('Waiting for connection')
+
+try:
+        ClientSocket.connect((host,port))
+
+except socket.error as e:
+        print(str(e))
+
+response = ClientSocket.recv(1024)
+
+print(response)
+
+while True:
+        Input = input("Say something : ")
+        ClientSocket.send(str.encode(Input))
+        response = ClientSocket.recv(1024)
+        print(response.decode('utf-8'))
+
+ClientSocket.close()
